c7c663baba3477d0196e1804dbc9db56afc2cda5
lbonfiglio <lukebonfiglio98@gmail.com>
2019-09-13 13:49:57 +1000

    Accidental Change - Has been restored

--- include/priors.hpp
+++ include/priors.hpp
@@ -1,3 +1,5 @@
+
+
 #ifndef PRIORS_HPP
 #define PRIORS_HPP
 
@@ -5,26 +7,26 @@
 
 
 namespace priors {
-  
+
   double flat(double unit, double min, double max) {
     return (max - min) * unit + min;
   }
-  
+
   double log(double unit, double min, double max) {
     return min * pow(max / min, unit);
   }
-  
+
   double gaussian(double unit, double mu, double sigma) {
     boost::math::normal dist(mu, sigma);
     return quantile(dist, unit);
   }
-  
+
   double signed_log(double unit, double min, double max) {
     double mapped = (unit < 0.5) ? 2. * unit : 2. * unit - 1.;
     double sign = (unit < 0.5) ? -1. : 1.;
     return sign * min * pow(max / min, mapped);
   }
-  
+
 }
 
 #endif  // PRIORS_HPP
